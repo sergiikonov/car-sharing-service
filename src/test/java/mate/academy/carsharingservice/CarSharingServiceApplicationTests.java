8438ffbbecddb5e8f598a999fdb93034205c4b13
Sergii Konovalov <sergii2konovalov@gmail.com>
2025-07-08 11:24:16 +0300

    created project

---
+++ src/test/java/mate/academy/carsharingservice/CarSharingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.carsharingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
